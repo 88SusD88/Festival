5a8ffe473fb049f7ca07ce909a01c0fd6a0e92a9
88SusD88 <is25suslinikovda@artcollege.ru>
2024-10-23 21:50:22 +0300

    Добавьте файлы проекта.

---
+++ Festival/FormAddActivity.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Festival
+{
+    partial class FormAddActivity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonAddActivity = new System.Windows.Forms.Button();
+            this.buttonAddJuri = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.dayTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.eventPlanIDComboBox = new System.Windows.Forms.ComboBox();
+            this.eventBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ModeratorComboBox = new System.Windows.Forms.ComboBox();
+            this.userBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.juriComboBox = new System.Windows.Forms.ComboBox();
+            this.userBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(12, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 0;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonAddActivity
+            // 
+            this.buttonAddActivity.Location = new System.Drawing.Point(12, 406);
+            this.buttonAddActivity.Name = "buttonAddActivity";
+            this.buttonAddActivity.Size = new System.Drawing.Size(200, 30);
+            this.buttonAddActivity.TabIndex = 1;
+            this.buttonAddActivity.Text = "Добавить активность";
+            this.buttonAddActivity.UseVisualStyleBackColor = true;
+            this.buttonAddActivity.Click += new System.EventHandler(this.buttonAddActivity_Click);
+            // 
+            // buttonAddJuri
+            // 
+            this.buttonAddJuri.Location = new System.Drawing.Point(218, 317);
+            this.buttonAddJuri.Name = "buttonAddJuri";
+            this.buttonAddJuri.Size = new System.Drawing.Size(123, 24);
+            this.buttonAddJuri.TabIndex = 2;
+            this.buttonAddJuri.Text = "Добавить";
+            this.buttonAddJuri.UseVisualStyleBackColor = true;
+            this.buttonAddJuri.Click += new System.EventHandler(this.buttonAddJuri_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "План события";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "День";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Начало мер.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Модератор";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Жюри";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(320, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Добавление активностей";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(15, 93);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(197, 22);
+            this.titleTextBox.TabIndex = 10;
+            // 
+            // dayTextBox
+            // 
+            this.dayTextBox.Location = new System.Drawing.Point(12, 183);
+            this.dayTextBox.Name = "dayTextBox";
+            this.dayTextBox.Size = new System.Drawing.Size(200, 22);
+            this.dayTextBox.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 227);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // eventPlanIDComboBox
+            // 
+            this.eventPlanIDComboBox.DataSource = this.eventBindingSource;
+            this.eventPlanIDComboBox.DisplayMember = "Title";
+            this.eventPlanIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.eventPlanIDComboBox.FormattingEnabled = true;
+            this.eventPlanIDComboBox.Location = new System.Drawing.Point(12, 137);
+            this.eventPlanIDComboBox.Name = "eventPlanIDComboBox";
+            this.eventPlanIDComboBox.Size = new System.Drawing.Size(200, 24);
+            this.eventPlanIDComboBox.TabIndex = 13;
+            this.eventPlanIDComboBox.ValueMember = "ID";
+            // 
+            // eventBindingSource
+            // 
+            this.eventBindingSource.DataSource = typeof(Festival.DBCon.Event);
+            // 
+            // ModeratorComboBox
+            // 
+            this.ModeratorComboBox.DataSource = this.userBindingSource;
+            this.ModeratorComboBox.DisplayMember = "Surname";
+            this.ModeratorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ModeratorComboBox.FormattingEnabled = true;
+            this.ModeratorComboBox.Location = new System.Drawing.Point(12, 271);
+            this.ModeratorComboBox.Name = "ModeratorComboBox";
+            this.ModeratorComboBox.Size = new System.Drawing.Size(200, 24);
+            this.ModeratorComboBox.TabIndex = 14;
+            this.ModeratorComboBox.ValueMember = "ID";
+            // 
+            // userBindingSource
+            // 
+            this.userBindingSource.DataSource = typeof(Festival.DBCon.User);
+            // 
+            // juriComboBox
+            // 
+            this.juriComboBox.DataSource = this.userBindingSource2;
+            this.juriComboBox.DisplayMember = "Surname";
+            this.juriComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.juriComboBox.FormattingEnabled = true;
+            this.juriComboBox.Location = new System.Drawing.Point(15, 317);
+            this.juriComboBox.Name = "juriComboBox";
+            this.juriComboBox.Size = new System.Drawing.Size(197, 24);
+            this.juriComboBox.TabIndex = 15;
+            this.juriComboBox.ValueMember = "ID";
+            // 
+            // userBindingSource2
+            // 
+            this.userBindingSource2.DataSource = typeof(Festival.DBCon.User);
+            // 
+            // FormAddActivity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 448);
+            this.Controls.Add(this.juriComboBox);
+            this.Controls.Add(this.ModeratorComboBox);
+            this.Controls.Add(this.eventPlanIDComboBox);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dayTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAddJuri);
+            this.Controls.Add(this.buttonAddActivity);
+            this.Controls.Add(this.buttonBack);
+            this.Name = "FormAddActivity";
+            this.Text = "Добавить активность";
+            this.Load += new System.EventHandler(this.FormAddActivity_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonAddActivity;
+        private System.Windows.Forms.Button buttonAddJuri;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox dayTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox eventPlanIDComboBox;
+        private System.Windows.Forms.ComboBox ModeratorComboBox;
+        private System.Windows.Forms.ComboBox juriComboBox;
+        private System.Windows.Forms.BindingSource eventBindingSource;
+        private System.Windows.Forms.BindingSource userBindingSource;
+        private System.Windows.Forms.BindingSource userBindingSource2;
+    }
+}
---
+++ Festival/MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Festival
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.activityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.activityDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonShowJuri = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.activityBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.activityDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // activityBindingSource
+            // 
+            this.activityBindingSource.DataSource = typeof(Festival.DBCon.Activity);
+            // 
+            // activityDataGridView
+            // 
+            this.activityDataGridView.AutoGenerateColumns = false;
+            this.activityDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.activityDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7});
+            this.activityDataGridView.DataSource = this.activityBindingSource;
+            this.activityDataGridView.Location = new System.Drawing.Point(17, 40);
+            this.activityDataGridView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.activityDataGridView.Name = "activityDataGridView";
+            this.activityDataGridView.RowHeadersWidth = 51;
+            this.activityDataGridView.RowTemplate.Height = 24;
+            this.activityDataGridView.Size = new System.Drawing.Size(1047, 456);
+            this.activityDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ID";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Title";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Title";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Day";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Day";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "StartedAt";
+            this.dataGridViewTextBoxColumn5.HeaderText = "StartedAt";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "ModeratorID";
+            this.dataGridViewTextBoxColumn6.HeaderText = "ModeratorID";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "GroupsJury";
+            this.dataGridViewTextBoxColumn7.HeaderText = "GroupsJury";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Таблица активностей";
+            // 
+            // buttonShowJuri
+            // 
+            this.buttonShowJuri.Location = new System.Drawing.Point(17, 503);
+            this.buttonShowJuri.Name = "buttonShowJuri";
+            this.buttonShowJuri.Size = new System.Drawing.Size(217, 34);
+            this.buttonShowJuri.TabIndex = 3;
+            this.buttonShowJuri.Text = "Показать жюри";
+            this.buttonShowJuri.UseVisualStyleBackColor = true;
+            this.buttonShowJuri.Click += new System.EventHandler(this.buttonShowJuri_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(240, 503);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(217, 34);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 549);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonShowJuri);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.activityDataGridView);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Главная форма";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.activityBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.activityDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource activityBindingSource;
+        private System.Windows.Forms.DataGridView activityDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonShowJuri;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
